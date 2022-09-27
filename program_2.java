56c059f18604011b6a68bd5512c973f61dcaad78
Praveen66678 <114468551+Praveen66678@users.noreply.github.com>
2022-09-27 14:04:00 +0530

    Create program_2.java

---
+++ program_2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Filesone
+{
+ public static void main(String args[])
+ {
+  Scanner s=new Scanner(System.in);
+  System.out.println("Enter the values :");
+  int n=s.nextInt();
+  
+  for(int i=1;i<=n;i++);
+  {
+   System.out.println(n);
+   }
+   }
+   }
