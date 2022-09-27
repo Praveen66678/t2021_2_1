6ba60c42ee64039329ce08dd51890521c580b1d7
Praveen66678 <114468551+Praveen66678@users.noreply.github.com>
2022-09-27 14:12:42 +0530

    Create program_3.java

---
+++ program_3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Example
+{
+ public static void main(String args[])
+ {
+  Scanner s=new Scanner(System.in);
+  System.out.println("Enter the Integer :");
+  int a=s.nextInt();
+  
+  for(int i=0;i<=a;i++)
+  {
+  if(i==a)
+  {
+   break;
+   }
+   System.out.println(a + i);
+   }
+   }
+   }
